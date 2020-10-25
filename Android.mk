19bab0121bed1d4f2d5dccdf23cc52bfa24ed04b
SpiritCroc <dev@spiritcroc.de>
2020-10-25 10:02:40 +0100

    Messaging: better dark
    Previously, we had overlays to do this, but let's do this here now, as
    we cannot reference original app resources since R.
    Resources imported from packages_overlays_AICP (and originally imported
    from the DarkCroc-Substratum theme).
    
    Change-Id: Iad42dbfdffff6753a227ddc4b2e770ab8c5da298

--- Android.mk
+++ Android.mk
@@ -19,7 +19,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/dark-res
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
